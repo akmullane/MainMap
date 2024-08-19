683977d39551ec1667ec935f02f797613e743410
Gain savagez <60669595+akmullane@users.noreply.github.com>
2024-08-18 21:29:04 -0400

    1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import { getMapData, show3dMap } from "@mappedin/mappedin-js";
+import { getMapData, show3dMap } from '@mappedin/mappedin-js';
 import "@mappedin/mappedin-js/lib/index.css";
+import "./styles.css";
 
-// See Demo API key Terms and Conditions
-// https://developer.mappedin.com/v6/demo-keys-and-maps/
-const options = {
-    key: '65ca6d27d53f21f234ae6395',
-    secret: '0b25fc24d564c644443663d0b4d083605090d349975d0983fc96e06a5b1934dd',
-    mapId: '65c0ff7430b94e3fabd5bb8c'
-};
+async function go() {
+    // See Trial API key Terms and Conditions
+    // https://developer.mappedin.com/web/v6/trial-keys-and-maps/
+    const mapData = await getMapData({
+        key: '65ca6d27d53f21f234ae6395',
+        secret: '0b25fc24d564c644443663d0b4d083605090d349975d0983fc96e06a5b1934dd',
+        mapId: '65c0ff7430b94e3fabd5bb8c',
+    });
 
-async function init() {
-    const mapData = await getMapData(options);
-    const mapView = await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
+    //Display the default map in the mappedin-map div.
+    await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
 }
 
-init();
+go();
