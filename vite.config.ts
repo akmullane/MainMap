684496657495669c3069dde66d546654923a2529
Gain savagez <60669595+akmullane@users.noreply.github.com>
2024-08-19 21:20:51 -0400

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/', // Adjust this if your app is served from a subpath
+    build: {
+        outDir: 'dist',
+    },
     server: {
         host: '0.0.0.0',
         port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
