799bb685c8e58e4354a0025dfb4d9057c5e5a614
Gain savagez <60669595+akmullane@users.noreply.github.com>
2024-08-19 20:36:03 -0400

    2

--- src/map.ts
+++ src/map.ts
@@ -1,17 +1,17 @@
-import Mappedin from '@mappedin/mappedin-js';
+import { getMapData, show3dMap } from "@mappedin/mappedin-js";
+import "@mappedin/mappedin-js/lib/index.css";
 
-export function initMap(containerId: string) {
-    const container = document.getElementById(containerId);
-    if (!container) return;
+// See Demo API key Terms and Conditions
+// https://developer.mappedin.com/v6/demo-keys-and-maps/
+const options = {
+    key: '65ca6d27d53f21f234ae6395',
+    secret: '0b25fc24d564c644443663d0b4d083605090d349975d0983fc96e06a5b1934dd',
+    mapId: '65c0ff7430b94e3fabd5bb8c'
+};
 
-    const mapView = new Mappedin.MapView({
-        apiKey: 'YOUR_MAPPEDIN_API_KEY',
-        clientId: 'YOUR_CLIENT_ID',
-        mapStyles: 'YOUR_MAP_STYLES',
-        mapContainer: container,
-    });
-
-    mapView.on('ready', () => {
-        console.log('Mappedin Map is ready!');
-    });
+async function init() {
+    const mapData = await getMapData(options);
+    const mapView = await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
 }
+
+init();
